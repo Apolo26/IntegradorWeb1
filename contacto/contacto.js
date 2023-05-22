0f9fdf66ee79d2185ef9a904919464ca797efef4
Ezequiel Lucero <carlosezequieldev@gmail.com>
2023-05-22 18:47:41 -0300

    nuevo commit
    agregando y validando el formulario

--- contacto/contacto.js
+++ contacto/contacto.js
@@ -3,10 +3,9 @@
 window.addEventListener("scroll", function () {
   var header = document.querySelector("header");
   header.classList.toggle("abajo", window.scrollY > 0);
-})
+});
 
-
-//preguntas frecuentes
+//Preguntas Frecuentes
 
 const contenidoAcordeon = document.querySelectorAll(".contenido-acordeon");
 
@@ -25,22 +24,91 @@ contenidoAcordeon.forEach((item, index) => {
       item.querySelector("i").classList.replace("fa-eye-slash", "fa-eye");
     }
 
-
-
-    removerAbierto(index)
-  })
-})
+    removerAbierto(index);
+  });
+});
 
 function removerAbierto(index1) {
   contenidoAcordeon.forEach((item2, index2) => {
-    if(index1 != index2) {
-      item2.classList.remove("abierto")
+    if (index1 != index2) {
+      item2.classList.remove("abierto");
 
-      let des = item2.querySelector(".descripcion")
-      des.style.height = "0px"
+      let des = item2.querySelector(".descripcion");
+      des.style.height = "0px";
       item2.querySelector("i").classList.replace("fa-eye-slash", "fa-eye");
     }
-  })
+  });
 }
 
 
+//Validacion de formulario
+
+
+const formulario = document.getElementById("formulario");
+const nombreInput = document.getElementById("nombre");
+const emailInput = document.getElementById("email");
+const telefonoInput = document.getElementById("telefono");
+const mensajeInput = document.getElementById("mensaje");
+
+formulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (validarCampos()) {
+
+    console.log("Formulario válido");
+  }
+});
+
+function validarCampos() {
+  const nombre = nombreInput.value.trim();
+  const email = emailInput.value.trim();
+  const telefono = telefonoInput.value.trim();
+  const mensaje = mensajeInput.value.trim();
+
+  if (nombre === "") {
+    alert("Por favor, ingresa tu nombre.");
+    nombreInput.focus();
+    return false;
+  }
+
+  if (email === "") {
+    alert("Por favor, ingresa tu dirección de correo electrónico.");
+    emailInput.focus();
+    return false;
+  }
+
+  if (!validarEmail(email)) {
+    alert("Por favor, ingresa una dirección de correo electrónico válida.");
+    emailInput.focus();
+    return false;
+  }
+
+  if (telefono === "") {
+    alert("Por favor, ingresa tu número de teléfono.");
+    telefonoInput.focus();
+    return false;
+  }
+
+  if (mensaje === "") {
+    alert("Por favor, ingresa tu mensaje.");
+    mensajeInput.focus();
+    return false;
+  }
+
+  return true;
+}
+
+function validarEmail(email) {
+  const regex = /^\S+@\S+\.\S+$/;
+  return regex.test(email);
+}
+
+telefonoInput.addEventListener("input", function (event) {
+  let telefono = telefonoInput.value;
+
+  telefono = telefono.replace(/\D/g, "");
+
+  telefono = telefono.slice(0, 10);
+
+  telefonoInput.value = telefono;
+});
